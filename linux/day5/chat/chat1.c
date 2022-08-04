88ab9a56857d3bcf827a9709398d0db959eb2894
Bpretty <liyao_19@outlook.com>
2022-08-04 10:24:59 +0800

    select版管道聊天

---
+++ linux/day5/chat/chat1.c
@@ -0,0 +1,51 @@
+#include <func.h>
+
+int main( int argc, char* argv[])
+{
+    if ( argc != 3){
+        printf("参数错误，%d", argc);
+        return -1;
+    }
+
+    int fdR, fdW;
+    printf("begin\n");
+    fdR = open(argv[1], O_RDONLY);
+    //printf("aa\n");
+    fdW = open(argv[2], O_WRONLY);
+    //printf("bb\n");
+    int ret = 0;
+    char buff[128] = {0};
+
+    fd_set fds;
+    while(1)
+    {
+        FD_ZERO(&fds );
+        FD_SET(STDIN_FILENO, &fds );
+        FD_SET(fdR, &fds);
+
+        int nNums = select( fdR+1, &fds, NULL, NULL, NULL  );
+        if (nNums > 0)
+        {
+            if (FD_ISSET(fdR, &fds))
+            {
+                memset( buff, 0, sizeof(buff) );
+                ret = read(fdR, buff, sizeof(buff) );
+                if (ret ==0)
+                {
+                    printf("再见\n");
+                    break;
+                }
+                //printf( "ret = %d, buff =%s\n",ret, buff );
+                printf("%s\n",buff);
+            }
+
+            if (FD_ISSET( STDIN_FILENO, &fds))
+            {
+                memset(buff, 0, sizeof(buff));
+                ret = read(STDIN_FILENO, buff, sizeof(buff) );
+                //printf("从标准输入读取字符：%d\n", ret);
+                ret =write( fdW, buff, strlen(buff)-1 );
+            }
+        }
+    }
+}
---
+++ linux/day5/chat/chat2.c
@@ -0,0 +1,53 @@
+#include <func.h>
+
+int main( int argc, char* argv[])
+{
+    if ( argc != 3){
+        printf("参数错误，%d", argc);
+        return -1;
+    }
+
+    int fdR, fdW;
+    printf("00\n");
+    fdW = open(argv[1],O_WRONLY );
+    //printf("11\n");
+    fdR = open(argv[2],O_RDONLY );
+    //printf("22\n");
+    int ret = 0;
+    char buff[128] = {0};
+
+    fd_set fds;
+    while(1)
+    {
+        FD_ZERO(&fds);
+        FD_SET( STDIN_FILENO, &fds );
+        FD_SET(fdR, &fds);
+
+        int nNums = select( fdR+1, &fds, NULL, NULL, NULL  );
+        if (nNums > 0)
+        {
+            if (FD_ISSET(STDIN_FILENO, &fds))
+            {
+                memset(buff, 0, sizeof(buff));
+                ret = read(STDIN_FILENO, buff, sizeof(buff) );
+                //ret = read(STDIN_FILENO, buff, strlen(buff)-1 );
+                //printf("从标准输入读取字符：%d\n", ret);
+                write( fdW, buff, strlen(buff)-1 );
+            }
+
+            if (FD_ISSET(fdR, &fds))
+            {
+                memset( buff, 0, sizeof(buff) );
+                ret = read(fdR, buff, sizeof(buff) );
+                if (ret ==0)
+                {
+                    printf("再见\n");
+                    break;
+                }
+                //printf( "ret = %d, buff =%s",ret, buff );
+                printf("%s\n", buff);
+            }
+        }
+
+   }
+}
